6e8595b1c9b22adf0ebc4ecdb3bd66f0dd02e111
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-04 21:23:46 +0500

    smit assignment 2

---
+++ smit assignment 15/app.js
@@ -0,0 +1,116 @@
+
+//        CHAPTER 2
+
+
+
+// // Q NO#1
+
+// var username = "Hussain"
+
+// // Q NO#2
+// username = "muhammad hussain"
+
+
+// // Q NO#3
+
+// var massage = "HELLO WORLD"
+// alert(massage)
+
+// // Q NO#4
+// var StudentBioData = "I am 23 years old"
+// alert(StudentBioData)
+
+// // Q NO#5
+
+// var pizza ="PIZZA \n PIZZ \n PIZ \n PI \n P"
+
+// alert ( pizza)
+
+// // Q NO#6
+
+// var email = "My email address is  hussain.batch9@gmail.com"
+// alert(email)
+
+// // Q NO#7
+// var books = "A smarter way to learn JavaScript"
+// alert(books)
+
+// // Q NO#8
+
+// document.write("<h1> Yah! I can write HTML content through Java Scripts </ h1>")
+
+
+// // Q NO#9
+
+// var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(end)
+
+
+
+// //    CHAPTER 3
+
+// //  Q NO#1
+
+// var age = 23
+// alert ("I am " + (age) +  " year old")
+
+
+// //  Q NO#2
+
+// var T = 14
+// alert("you have visited this side" + (T) + " times")
+
+// //  Q NO#3
+
+// var birthYear = 1999
+// document.write("my birth year is " +  birthYear)
+
+// //  Q NO#4
+// var visitorName = "Jhon doe" 
+// var productTitle = "T-shirt"
+// var quantity ="5"
+// document.write(visitorName + " orderd "+ quantity + productTitle + "(s) on XYZ Clothing" ) 
+
+
+//    CHAPTER 4
+
+// //  Q NO#1
+
+// var name1 = "hussain", name2 ="faizan",  name3 = "hamza";
+// console.log(name1,name2,name3)
+
+// //  Q NO#2
+
+//    // legal 
+// var saylaniWelfare = "trust" 
+// var sir = "ghous ahmed"
+// var country = "pakistan"
+// var city = "karachi"
+// var province = "sindh"
+// console.log(saylaniWelfare,sir,country,city,province)
+
+//     // illegal
+// var %saylaniWelfare = "trust" 
+// var #sir = "ghous ahmed"
+// var @country = "pakistan"
+// var !city = "karachi"
+// var &province = "sindh"
+
+
+// // Q NO#3
+
+// document.write("<h1> Rules for naming JS variables </h1>")
+
+
+// var num = "number"
+// var dol = "$"
+// var nd = "and"
+// var desh = "_"
+// var let = "letter"
+// var sen = "sensitive"
+// var key = " keyword"
+ 
+// document.write("Variable names can only contain" + " " + num + " " + dol + " " + nd + " " + "and " + " " +  desh + "." ) 
+// document.write("Variable names can only contain" + " " + let + " " + dol + " " + "or " + " " +  desh + ".")
+// document.write( "variable names are case" + " " + sen + ".")
+// document.write("variable names should not be" + " " + key + "." )
